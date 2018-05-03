94170a078140ae5bd0aaf7567b8063eded3f8f76
artsdeep <artsdeep@gmail.com>
2018-05-03 20:16:07 +0300

    update

---
+++ models.py
@@ -0,0 +1,10 @@
+from webapp import db
+
+class Base(db.Model):
+    __abstract__  = True
+    id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.DateTime,  default=db.func.current_timestamp())
+
+class Client(Base):
+    __tablename__ = 'client'
+    name = db.Column(db.String(128),  nullable=False)
--- webapp.py
+++ webapp.py
@@ -43,15 +43,6 @@ app = Flask(__name__) # pylint: disable=invalid-name
 
 db = SQLAlchemy(app)
 
-class Base(db.Model):
-    __abstract__  = True
-    id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(db.DateTime,  default=db.func.current_timestamp())
-
-class Client(Base):
-    __tablename__ = 'client'
-    name = db.Column(db.String(128),  nullable=False)
-
 db.drop_all()
 db.create_all()
 
